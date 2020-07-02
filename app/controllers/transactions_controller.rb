02f0839102094ed5b52da9a5ea5d5165fcb6179d
Monique Rio <mrio@umich.edu>
2020-07-02 15:24:43 -0400

    started modifying transactions to spit out the requested info

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,34 @@
 class TransactionsController < ApplicationController
   def index
-    url ="#{ENV.fetch('ALMA_API_HOST')}/almaws/v1/users?apikey=#{ENV.fetch('ALMA_API_KEY')}" 
-    users = request_api(url)    
-    render json: users
+    url ="users/#{params[:id]}/loans"
+    loans = request_api(url)    
+    transactions = loans['item_loan'].map  do |l|
+      [
+        'duedate' => l['due_date'],
+        'isbn'    => '',
+        'status'      => '',
+        'author'  => l['author'],
+        'title'   => l['title'],
+        'barcode'   => l['item_barcode'],
+        'call_number' => '', 
+        'description' => '', 
+        'id'          => '', 
+        'bib_library' => '', 
+        'location'    => '', 
+        'format'      => '', 
+        'num'         => '', 
+  
+      ]
+    end
+    render json: transactions
   end
 
   def request_api(url)
+    url = "#{ENV.fetch('ALMA_API_HOST')}/almaws/v1/#{url}"
     response = Excon.get(
       url,
       headers: {
+        'Authorization' => "apikey #{ENV.fetch('ALMA_API_KEY')}",
         'accept' => 'application/json'
       }
     ) 
