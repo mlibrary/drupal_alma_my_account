4911e3473f2a5a69a037f7895f938491b79d770b
Monique Rio <mrio@umich.edu>
2020-07-06 10:46:26 -0400

    transactions closer to what they should be

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,23 +2,23 @@ class TransactionsController < ApplicationController
   def index
     url ="users/#{params[:id]}/loans"
     loans = request_api(url)    
-    transactions = loans['item_loan'].map  do |l|
-      [
-        'duedate' => l['due_date'],
-        'isbn'    => '',
-        'status'      => '',
-        'author'  => l['author'],
-        'title'   => l['title'],
-        'barcode'   => l['item_barcode'],
-        'call_number' => '', 
-        'description' => '', 
-        'id'          => '', 
-        'bib_library' => '', 
-        'location'    => '', 
-        'format'      => '', 
-        'num'         => '', 
+    transactions = loans['item_loan'].map.with_index do |loan, index|
+      { 
+        'duedate' => loan['due_date'],
+        'isbn'    => '', #get from item record
+        'status'      => '', #must be calculated
+        'author'  => loan['author'],
+        'title'   => loan['title'],
+        'barcode'   => loan['item_barcode'],
+        'call_number' => loan['call_number'], 
+        'description' => loan['description'], 
+        'id'          => loan['mms_id'], 
+        'bib_library' => '',  #don't know how this will work in Alma (MIU01/MIU30)
+        'location'    => loan['library']['desc'], 
+        'format'      => '', #get from item record
+        'num'         => index, 
   
-      ]
+      }
     end
     render json: transactions
   end
